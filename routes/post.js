e519095fd73ab3676406acb45e583297a7c81985
Your Name <you@example.com>
2023-12-03 15:50:44 +0000

    Update post API

--- routes/post.js
+++ routes/post.js
@@ -32,7 +32,7 @@ router.post('/createPost', verifyToken, async (req, res) => {
     }
 })
 
-router.get('/getAllPosts', async (req, res) => {
+router.get('/getAllPosts', verifyToken, async (req, res) => {
     try {
         console.log(".......................................................")
         const posts = await Post.find()
@@ -43,24 +43,44 @@ router.get('/getAllPosts', async (req, res) => {
     }
 })
 
-router.put('/updatePost', async (req, res) => {
-    const {postId} = req.query()
+router.put('/updatePost', verifyToken,async (req, res) => {
+    const {postId} = req.query
     const {postTitle, postTopic, message} = req.body
     const loggedUser = req.user.username;
 
     try {
+
         const postExists = await Post.findOne({post_id: postId})
 
         if(!postExists) {
-            res.status(401).json({error: "Post not found"})
+            return res.status(404).json({error: "Post not found"})
+        }
+        // updatedPost = new Post({
+        //     post_title: req.body.post_title,
+        //     post_topic: req.body.post_topic,
+        //     message: req.body.message,
+        // })
+
+        if(postExists.postOwner != loggedUser) {
+            return res
+            .status(403)
+            .json({error: "Access Denied. You do not have the permission to modify this post"})
         }
 
-        // if(postExists.posstOwner != loggedUser) {
+        if(postExists.expiration_time < Date.now) {
+            return res.status(403).json({error: "Post expired for update"})
+        }
 
-        // }
+        postExists.title = postTitle || postExists.title
+        postExists.post_topic = postTopic || postExists.post_topic
+        postExists.message = message || postExists.message
+
+        await postExists.save();
+        res.status(200).json({message: "Post updated successfully."})
     }
     catch {
-
+        console.error(error);
+        res.status(500).json({ error: "Internal server error"})
     }
 })
 
